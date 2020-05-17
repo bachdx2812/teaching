9a132c8d83aa7e59c4b95ff7eb8acbad34406c03
bachdx <bachdx.hut@gmail.com>
2020-05-17 08:48:03 +0700

    btvn1

--- js/buoi1/teach/index.js
+++ js/buoi1/teach/index.js
@@ -1,14 +1,48 @@
 // tao event click cho button t
-sayHi = function() {
+calculate = function() {
   // khi click get input value from
-  const name = document.getElementById("name").value;
+  const number1 = document.getElementById("number1").value;
+  const number2 = document.getElementById("number2").value;
+  const operator = document.getElementById("operator").value;
 
-  const contentSection = document.getElementById("content");
-  // dua content vao trong content section
-  contentSection.innerHTML = name;
-  // hien content section
-  contentSection.style.display = "block";
+  if (
+    operator !== "+" && operator !== "-" && operator !== '*' && operator !== '/'
+  ) {
+    alert("loi")
+  } else {
+    const floatBreaker = 100000;
 
-  // sau do thi an cai input section
-  document.getElementById("input").style.display = "none";
+    switch(operator) {
+      case "+":
+        console.log("cong")
+        console.log(parseFloat(number1) + parseFloat(number2));
+
+        break;
+      case "-":
+        console.log("tru")
+
+        const result = (floatBreaker * parseFloat(number1) - floatBreaker * parseFloat(number2)) / floatBreaker;
+        console.log(result);
+        break;
+      case "*":
+        console.log("nhan");
+        console.log(parseFloat(number1) * parseFloat(number2));
+
+        break;
+      case "/":
+        console.log("chia")
+        console.log(parseFloat(number1) / parseFloat(number2))
+
+        break;
+    }
+  }
+
+  // const contentSection = document.getElementById("content");
+  // // dua content vao trong content section
+  // contentSection.innerHTML = name;
+  // // hien content section
+  // contentSection.style.display = "block";
+
+  // // sau do thi an cai input section
+  // document.getElementById("input").style.display = "none";
 }
---
+++ js/buoi2/teach/index.js
@@ -0,0 +1,9 @@
+// function definition
+function calculateBill() {
+  // this is the function body
+  // `calculateBill` is the NAME of the function
+
+}
+
+// function call ( run / execute / invoke )
+calculateBill();
