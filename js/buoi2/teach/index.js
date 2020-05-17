e45b9e88c486332801748bee4eefc6ebdf3f83f4
bachdx <bachdx.hut@gmail.com>
2020-05-17 10:00:24 +0700

    teach b2

--- js/buoi2/teach/index.js
+++ js/buoi2/teach/index.js
@@ -1,9 +1,26 @@
-// function definition
-function calculateBill() {
-  // this is the function body
-  // `calculateBill` is the NAME of the function
+/* 
+  function as an argument
+*/ 
 
+// function callAMan(name) {
+//   return `Mr ${name.toUpperCase()}`;
+// }
+
+// function giveThisManAShield(name) {
+//   console.log(`Give ${name} a fucking shield`);
+// }
+
+// giveThisManAShield(callAMan("bach"));
+
+/* 
+  Default parameter's value
+*/ 
+
+function getRealPrice(price, taxRate = 0.1, shippingFee) {
+  return price + price * taxRate + shippingFee;
 }
+// getRealPrice(1000, undefined, 20);
 
-// function call ( run / execute / invoke )
-calculateBill();
+function getRealPrice1(price, taxRate = 0.1, shippingFee = 30) {
+  return price + price * taxRate + shippingFee;
+}
